902816c56d23973dba7be0cbb5f5f89982bce82b
lulski <milunes@gmail.com>
2017-05-08 23:46:34 +1000

    1

---
+++ gs-rest-service/initial/src/main/java/com/lulski/budgeta/domain/Budget.java
@@ -0,0 +1,13 @@
+package com.lulski.budgeta.domain;
+
+public class Budget {
+	private float amount;
+
+	public float getAmount() {
+		return amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	} 
+}
